797dd9e507080dfd84ca4aa677896ba1f14ee32d
patrykos <patryka@email.com>
2023-06-09 23:10:43 -0500

    Setting up Redux

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,12 +1,13 @@
-import { StyleSheet,Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
+import { baseUrl } from '../../shared/baseUrl';
 
 const RenderCampsite = (props) => {
     const { campsite } = props;
     if (campsite) {
         return (
             <Card containerStyle={styles.cardContainer}>
-                <Card.Image source={campsite.image}>
+                <Card.Image source={{ uri: baseUrl + campsite.image }}>
                     <View style={{ justifyContent: 'center', flex: 1 }}>
                         <Text
                             style={{
@@ -26,10 +27,10 @@ const RenderCampsite = (props) => {
                     color='#f50'
                     raised
                     reverse
-                    onPress={() => 
+                    onPress={() =>
                         props.isFavorite
-                            ? console.log('Already set as a favorite') 
-                            :  props.markFavorite()
+                            ? console.log('Already set as a favorite')
+                            : props.markFavorite()
                     }
                 />
             </Card>
@@ -37,6 +38,7 @@ const RenderCampsite = (props) => {
     }
     return <View />;
 };
+
 const styles = StyleSheet.create({
     cardContainer: {
         padding: 0,
--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,31 +1,29 @@
-import RenderCampsite from '../features/campsites/RenderCampsite';
 import { useState } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { COMMENTS } from '../shared/comments';
+import { useSelector } from 'react-redux';
+import RenderCampsite from '../features/campsites/RenderCampsite';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
+    const comments = useSelector((state) => state.comments);
 
-    const [comments, setComments] = useState(COMMENTS);
     const [favorite, setFavorite] = useState(false);
 
     const renderCommentItem = ({ item }) => {
         return (
             <View style={styles.commentItem}>
                 <Text style={{ fontSize: 14 }}>{item.text}</Text>
-                <Text style={{ fontSize: 12 }}>{item.rating}</Text>
+                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
                 <Text style={{ fontSize: 12 }}>
-                    {`--${item.author}, ${item.date}`}
+                    {`-- ${item.author}, ${item.date}`}
                 </Text>
             </View>
-
-
-        )
-    }
+        );
+    };
 
     return (
         <FlatList
-            data={comments.filter(
+            data={comments.commentsArray.filter(
                 (comment) => comment.campsiteId === campsite.id
             )}
             renderItem={renderCommentItem}
@@ -36,7 +34,7 @@ const CampsiteInfoScreen = ({ route }) => {
             }}
             ListHeaderComponent={
                 <>
-                    <RenderCampsite 
+                    <RenderCampsite
                         campsite={campsite}
                         isFavorite={favorite}
                         markFavorite={() => setFavorite(true)}
@@ -46,7 +44,6 @@ const CampsiteInfoScreen = ({ route }) => {
             }
         />
     );
-
 };
 
 const styles = StyleSheet.create({
@@ -64,6 +61,6 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         backgroundColor: '#fff'
     }
-})
+});
 
 export default CampsiteInfoScreen;

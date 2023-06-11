4b828b5a7b831a1e85ec6844abe4335bd1ed7993
patrykos <patryka@email.com>
2023-06-11 16:25:15 -0500

    Finished Task nr 3

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -4,6 +4,7 @@ import RenderCampsite from '../features/campsites/RenderCampsite';
 import { toggleFavorite } from '../features/favorites/favoritesSlice';
 import { useState } from 'react';
 import { Rating, Input, } from 'react-native-elements';
+import { postComment } from '../features/comments/commentsSlice';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
@@ -23,6 +24,7 @@ const CampsiteInfoScreen = ({ route }) => {
             text,
             campsiteId: campsite.id
         };
+        dispatch(postComment(newComment));
         setShowModal(!showModal);
     }
 
